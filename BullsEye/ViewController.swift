4d767433056e4ab5584bc3534362baf22b7e1200
tallpants <adithyakreddy6@gmail.com>
2018-07-14 12:36:47 +0530

    Capture and show slider value as integer

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,9 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let message = "The value of the slider is: \(currentValue)"
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
