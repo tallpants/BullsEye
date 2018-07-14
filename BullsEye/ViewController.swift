3e4bba2172f14ae5cfd6c5bd84a2fa4dc16ae434
tallpants <adithyakreddy6@gmail.com>
2018-07-14 13:10:53 +0530

    current value and target value + Slider IBOutlet

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,15 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue = 0
-
+    var targetValue = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        slider.value = Float(currentValue)
+        targetValue = 1 + Int(arc4random_uniform(100))
     }
 
     @IBAction func sliderMoved(_ slider: UISlider) {
@@ -27,11 +26,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)\nThe target value is: \(targetValue)" 
         let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
 }
-
